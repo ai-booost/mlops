b6750ed62f0834812a69e3882b9d3163fb82b410
wooobo <irooori89@gmail.com>
2025-01-16 16:57:34 +0900

    refactor: 코드 lint 정리

--- src/dataset/weather_requests.py
+++ src/dataset/weather_requests.py
@@ -1,8 +1,15 @@
-from datetime import datetime, timedelta
+"""
+기상청 요청 파라미터
+"""
+from datetime import datetime
 
 from dateutil.relativedelta import relativedelta
 
 class WeatherRequests:
+    """
+    기상청 요청 파라미터
+    """
+    # pylint: disable=R0903
     def __init__(self, start_time: datetime, end_time: datetime, location_id: str = 108):
         self.start_time = start_time
         self.end_time = end_time
@@ -11,5 +18,11 @@ class WeatherRequests:
 
     @staticmethod
     def create_month_range(start_time: datetime, month_range: int):
+        """
+        Create a WeatherRequests object with a month range.
+        :param start_time:
+        :param month_range:
+        :return:
+        """
         end_time = start_time + relativedelta(months=month_range)
         return WeatherRequests(start_time, end_time)
--- weather_collect.py
+++ weather_collect.py
@@ -1,15 +1,21 @@
+"""
+이 모듈은 기상청(KMA)의 날씨 데이터를 가져와 데이터 준비 과정을 자동화합니다.
+사용자가 시작 연도, 시작 월, 가져올 개월 수, 출력 파일 경로를 입력하면 데이터를 처리하여 CSV 파일로 저장합니다.
+"""
 import os
 import sys
 from datetime import datetime
+
 import fire
 from dotenv import load_dotenv
 from icecream import ic
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 from src.dataset.weather_requests import WeatherRequests
 from src.dataset.weather_datasource import WeatherDataSource
 from src.dataset.kma_client import KmaApiClient
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 
 def run_data_prepare(start_year, start_month, month_range_count, output_file):
     """
@@ -25,10 +31,12 @@ def run_data_prepare(start_year, start_month, month_range_count, output_file):
         raise ValueError("KMA_API_KEY가 설정되지 않았습니다.")
 
     weather_data_source: WeatherDataSource = KmaApiClient(auth_key)
-    request_params = WeatherRequests.create_month_range(datetime(start_year, start_month, 1), month_range_count)
+    start_time = datetime(start_year, start_month, 1)
+    request_params = WeatherRequests.create_month_range(start_time, month_range_count)
     response = weather_data_source.fetch_monthly_range_data(request_params)
     response.to_csv(output_file, index=False)
     ic(f"데이터가 {output_file}에 저장되었습니다.")
+
 
 if __name__ == "__main__":
     fire.Fire({
